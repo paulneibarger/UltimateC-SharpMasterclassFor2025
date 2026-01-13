4ee20f7458052f74cb975e3210a75bdb9d80e986
paulneibarger <2487669+paulneibarger@users.noreply.github.com>
2026-01-13 15:20:22 -0600

    Add ToDo Project Add greeting and menu

---
+++ ToDoList/ToDoList/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("What do you  want to do?");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+Console.ReadKey();
+
