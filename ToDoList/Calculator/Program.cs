5e585e303878df46580b6faf3632f58f81a91677
paulneibarger <2487669+paulneibarger@users.noreply.github.com>
2026-01-20 15:02:19 -0600

    Add assignment project call calculator

---
+++ ToDoList/Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello!");
+Console.WriteLine("Input the first number:");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Input the second number:");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("What would you like to do with those numbers?");
+Console.WriteLine("[A]dd");
+Console.WriteLine("[S]ubtract");
+Console.WriteLine("[M]ultiply");
+string operandType = Console.ReadLine().ToUpper();
+
+if (operandType == "A")
+{
+    Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+}
+else if (operandType == "S")
+{
+    Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+}
+else if (operandType == "M")
+{
+    Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+}
+
+Console.WriteLine("Press any key to close...");
+Console.ReadKey();
+
+
--- ToDoList/ToDoList/Program.cs
+++ ToDoList/ToDoList/Program.cs
@@ -1,33 +1,11 @@
-﻿Console.WriteLine("Hello!");
+﻿using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello!");
 Console.WriteLine("What do you  want to do?");
 Console.WriteLine("[S]ee all TODOs");
 Console.WriteLine("[A]dd a TODO");
 Console.WriteLine("[R]emove a TODO");
 Console.WriteLine("[E]xit");
 
-string userInput = Console.ReadLine().ToUpper();
+string userChoice = Console.ReadLine().ToUpper();
 
-if (userInput == "S")
-{
-    PrintSelectedOption("See all TODOs");
-}
-else if (userInput == "A")
-{
-    PrintSelectedOption("Add a TODOs");
-}
-else if (userInput == "R")
-{
-    PrintSelectedOption("Remove a TODOs");
-}
-else if (userInput == "E")
-{
-    PrintSelectedOption("Exit");
-}
-// to keep Windows terminal open
-Console.ReadKey();
-
-// Method to display what option the user selected
-void PrintSelectedOption(string selectedOption)
-{
-    Console.WriteLine($"Selected option: {selectedOption}");
-}
